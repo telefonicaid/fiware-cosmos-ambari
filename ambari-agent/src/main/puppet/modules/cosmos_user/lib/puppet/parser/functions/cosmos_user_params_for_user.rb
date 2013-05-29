96925bf5146d947fb80381cf1ed7ca6c694abb9b
Adamos Loizou <adamos@tid.es>
2013-05-29 22:49:22 +0200

    Fixed Ambari hdp::directory creation glitch Generalised cosmos-user to allow for N users.

---
+++ ambari-agent/src/main/puppet/modules/cosmos_user/lib/puppet/parser/functions/cosmos_user_params_for_user.rb
@@ -0,0 +1,29 @@
+# Telefónica Digital - Product Development and Innovation
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+# All rights reserved.
+
+module Puppet::Parser::Functions
+  newfunction(:cosmos_user_params_for_user, :type => :rvalue) do |args|
+    userIndex = args[0]
+    allUserParams = args[1]
+    thisUserParams = Hash[
+        Hash[
+            allUserParams.select {|key, _value| key =~ (/user#{userIndex}.*/)}
+        ].map{|key, value| [key.match(/user#{userIndex}_(.*)/)[1], value]}
+    ]
+
+    thisUserParams['user_home'] = "/home/#{thisUserParams['username']}"
+    thisUserParams['user_ssh_dir'] = "#{thisUserParams['user_home']}/.ssh"
+
+    thisUserParams['ssh_private_key_file'] = "#{thisUserParams['user_ssh_dir']}/id_rsa"
+    thisUserParams['ssh_public_key_file'] = "#{thisUserParams['user_ssh_dir']}/id_rsa.pub"
+    thisUserParams['ssh_authorized_keys_file'] = "#{thisUserParams['user_ssh_dir']}/authorized_keys"
+    thisUserParams['hdfs_user_dir'] = "/user/#{thisUserParams['username']}"
+    thisUserParams
+  end
+end
--- ambari-agent/src/main/puppet/modules/cosmos_user/manifests/user_slave_manager.pp
+++ ambari-agent/src/main/puppet/modules/cosmos_user/manifests/user_slave_manager.pp
@@ -1,3 +1,12 @@
+# Telefónica Digital - Product Development and Innovation
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+# All rights reserved.
+
 class cosmos_user::user_slave_manager($service_state) inherits cosmos_user::params {
 
   #adds user, directories, and common configs
@@ -9,9 +18,18 @@ class cosmos_user::user_slave_manager($service_state) inherits cosmos_user::para
   # only set slave content if master is not also slave (1-node cluster configuration)
   # so as not to replace master authorized keys
   if ($cosmos_user::params::master != $hostname) {
-    cosmos_user::authorized_keys{ "${cosmos_user::params::user}_slave_authorized_keys":
-      content => $cosmos_user::params::ssh_slave_authorized_keys,
+    #for each user
+    user_slave_keys {$cosmos_user::params::users_preambles:}
+  }
+
+  # User's authorized ssh keys on slave machine
+  define user_slave_keys {
+    $params_for_user = cosmos_user_params_for_user($name, $cosmos_user_config)
+
+    cosmos_user::authorized_keys{ "${$params_for_user['username']}_slave_authorized_keys":
+      content => $params_for_user['ssh_slave_authorized_keys'],
       service_state => $service_state,
+      params_hash => $params_for_user,
     }
   }
 }
