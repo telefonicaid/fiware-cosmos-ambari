61f0fe8d3cea13205c331d4d99c304ae72fc44cd
Adamos Loizou <adamos@tid.es>
2014-03-19 16:10:55 +0100

    Changed infinity-server configuration to receive datanode port and generate the url.

--- ambari-agent/src/main/puppet/modules/infinityfs_server/manifests/params.pp
+++ ambari-agent/src/main/puppet/modules/infinityfs_server/manifests/params.pp
@@ -16,7 +16,8 @@ class infinityfs_server::params inherits hdp::params {
   $server_port_master              = $infinity_configuration['server_port_master']
   $server_port_slave               = $infinity_configuration['server_port_slave']
   $proxy_secure_phrase_template    = $infinity_configuration['proxy_secure_phrase_template']
-  $hdfs_datanode_address           = $infinity_configuration['hdfs_datanode_address']
+  $hdfs_datanode_port              = $infinity_configuration['hdfs_datanode_port']
+  $hdfs_datanode_address           = "http://${::ipaddress}:${hdfs_datanode_port}"
 
   $allowed_sources                 = $hdp::params::all_hosts
   $package_and_service_name_master = 'infinity-server'
